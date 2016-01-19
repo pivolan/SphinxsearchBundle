f335c9e0f4010c1b6d5352bd0970e9395acb15bf
David Phruksukarn <david.phruksukarn@arestravelinc.com>
2016-01-18 16:23:23 -0800

    update di for sphinx lib

--- Search/Sphinxsearch.php
+++ Search/Sphinxsearch.php
@@ -14,33 +14,33 @@ use IAkumaI\SphinxsearchBundle\Doctrine\BridgeInterface;
  * @method string GetLastError ()
  * @method string GetLastWarning ()
  * @method bool IsConnectError()
- * @method null SetServer ( $host, $port = 0 )
- * @method null SetConnectTimeout ( $timeout )
+ * @method null SetServer ($host, $port = 0)
+ * @method null SetConnectTimeout ($timeout)
  *
- * @method null SetLimits ( $offset, $limit, $max=0, $cutoff=0 )
- * @method null setMaxQueryTime ( $max )
- * @method null SetMatchMode ( $mode )
- * @method null SetSortMode ( $mode, $sortby="" )
- * @method null SetFieldWeights ( $weights )
- * @method null SetIndexWeights ( $weights )
- * @method null SetIDRange ( $min, $max )
- * @method null SetFilter ( $attribute, $values, $exclude=false )
- * @method null SetFilterRange ( $attribute, $min, $max, $exclude=false )
- * @method null SetFilterFloatRange ( $attribute, $min, $max, $exclude=false )
- * @method null SetGeoAnchor ( $attrlat, $attrlong, $lat, $long )
- * @method null SetGroupBy ( $attribute, $func, $groupsort="@group desc" )
- * @method null SetGroupDistinct ( $attribute )
- * @method null SetRetries ( $count, $delay=0 )
- * @method null SetArrayResult ( $arrayresult )
- * @method null SetOverride ( $attrname, $attrtype, $values )
- * @method null SetSelect ( $select )
+ * @method null SetLimits ($offset, $limit, $max = 0, $cutoff = 0)
+ * @method null setMaxQueryTime ($max)
+ * @method null SetMatchMode ($mode)
+ * @method null SetSortMode ($mode, $sortby = "")
+ * @method null SetFieldWeights ($weights)
+ * @method null SetIndexWeights ($weights)
+ * @method null SetIDRange ($min, $max)
+ * @method null SetFilter ($attribute, $values, $exclude = false)
+ * @method null SetFilterRange ($attribute, $min, $max, $exclude = false)
+ * @method null SetFilterFloatRange ($attribute, $min, $max, $exclude = false)
+ * @method null SetGeoAnchor ($attrlat, $attrlong, $lat, $long)
+ * @method null SetGroupBy ($attribute, $func, $groupsort = "@group desc")
+ * @method null SetGroupDistinct ($attribute)
+ * @method null SetRetries ($count, $delay = 0)
+ * @method null SetArrayResult ($arrayresult)
+ * @method null SetOverride ($attrname, $attrtype, $values)
+ * @method null SetSelect ($select)
  *
  * @method null ResetFilters ()
  * @method null ResetGroupBy ()
  * @method null ResetOverrides ()
  *
- * @method bool|array BuildExcerpts ( $docs, $index, $words, $opts=array() )
- * @method bool|array BuildKeywords ( $query, $index, $hits )
+ * @method bool|array BuildExcerpts ($docs, $index, $words, $opts = array())
+ * @method bool|array BuildKeywords ($query, $index, $hits)
  *
  * @method array Status ()
  */
@@ -74,21 +74,17 @@ class Sphinxsearch
     /**
      * Constructor
      *
-     * @param string $host Sphinx host
-     * @param string $port Sphinx port
+     * @param string $host   Sphinx host
+     * @param string $port   Sphinx port
      * @param string $socket UNIX socket. Not required
      *
      * @throws NoSphinxAPIException If class SphinxClient does not exists
      */
     public function __construct($host, $port, $socket = null)
     {
-        $this->host = $host;
-        $this->port = $port;
+        $this->host   = $host;
+        $this->port   = $port;
         $this->socket = $socket;
-
-        if (!class_exists('SphinxClient')) {
-            throw new NoSphinxAPIException('You should include PHP SphinxAPI');
-        }
 
         $this->sphinx = new SphinxClient();
 
@@ -96,16 +92,6 @@ class Sphinxsearch
             $this->sphinx->setServer($this->socket);
         } else {
             $this->sphinx->setServer($this->host, $this->port);
-        }
-
-        if (!defined('SEARCHD_OK')) {
-            // To prevent notice
-            define('SEARCHD_OK', 0);
-        }
-
-        if (!defined('SEARCHD_WARNING')) {
-            // To prevent notice
-            define('SEARCHD_WARNING', 3);
         }
     }
 
@@ -153,15 +139,33 @@ class Sphinxsearch
         $results = $this->sphinx->query($query, $qIndex);
 
         if (!is_array($results)) {
-            throw new \RuntimeException(sprintf('Searching for "%s" failed. Result is not an array. Error "%s"', $query, $this->sphinx->getLastError()));
+            throw new \RuntimeException(
+                sprintf(
+                    'Searching for "%s" failed. Result is not an array. Error "%s"',
+                    $query,
+                    $this->sphinx->getLastError()
+                )
+            );
         }
 
         if (!isset($results['status'])) {
-            throw new \RuntimeException(sprintf('Searching for "%s" failed. Result with no status. Error "%s"', $query, $this->sphinx->getLastError()));
+            throw new \RuntimeException(
+                sprintf(
+                    'Searching for "%s" failed. Result with no status. Error "%s"',
+                    $query,
+                    $this->sphinx->getLastError()
+                )
+            );
         }
 
-        if ($results['status'] !== SEARCHD_OK && $results['status'] !== SEARCHD_WARNING) {
-            throw new \RuntimeException(sprintf('Searching for "%s" failed. Result has bad status. Error "%s"', $query, $this->sphinx->getLastError()));
+        if ($results['status'] !== SphinxClient::SEARCHD_OK && $results['status'] !== SphinxClient::SEARCHD_WARNING) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Searching for "%s" failed. Result has bad status. Error "%s"',
+                    $query,
+                    $this->sphinx->getLastError()
+                )
+            );
         }
 
         return $results;
@@ -169,9 +173,9 @@ class Sphinxsearch
 
     /**
      * Search for a query string and convert results to entities
-     * @param  string  $query   Search query
-     * @param  string|array   $index Index name(s) for search
-     * @param  boolean $escape  Should the query to be escaped?
+     * @param  string       $query  Search query
+     * @param  string|array $index  Index name(s) for search
+     * @param  boolean      $escape Should the query to be escaped?
      *
      * @return array           Search results
      *
@@ -204,8 +208,8 @@ class Sphinxsearch
     /**
      * Adds a query to a multi-query batch
      *
-     * @param string $query Search query
-     * @param array $indexes Index list to perform the search
+     * @param string $query   Search query
+     * @param array  $indexes Index list to perform the search
      *
      * @throws EmptyIndexException If $indexes is empty
      */
@@ -223,7 +227,7 @@ class Sphinxsearch
      * If set only one date it also works
      * If not one date is set method do nothing
      *
-     * @param string $attr Timestamp attr
+     * @param string    $attr      Timestamp attr
      * @param \DateTime $datestart Date to start filter
      * @param \DateTime $dateend   Date to end filter
      */
@@ -231,19 +235,22 @@ class Sphinxsearch
     {
         if ($datestart && $dateend) {
             $tsStart = (int)$datestart->format('U');
-            $tsEnd = (int)$dateend->format('U');
+            $tsEnd   = (int)$dateend->format('U');
 
-            $this->getClient()->setFilterRange($attr, $tsStart, $tsEnd);
+            $this->getClient()
+                 ->setFilterRange($attr, $tsStart, $tsEnd);
         } elseif ($datestart) {
             $tsStart = (int)$datestart->format('U');
-            $tsEnd = PHP_INT_MAX;
+            $tsEnd   = PHP_INT_MAX;
 
-            $this->getClient()->setFilterRange($attr, $tsStart, $tsEnd);
+            $this->getClient()
+                 ->setFilterRange($attr, $tsStart, $tsEnd);
         } elseif ($dateend) {
             $tsStart = 0;
-            $tsEnd = (int)$dateend->format('U');
+            $tsEnd   = (int)$dateend->format('U');
 
-            $this->getClient()->setFilterRange($attr, $tsStart, $tsEnd);
+            $this->getClient()
+                 ->setFilterRange($attr, $tsStart, $tsEnd);
         }
     }
 
